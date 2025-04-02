b9872cd38d692764997b243b0c47edc6fe9944a8
saimanideep <saimanideep765@gmail.com>
2025-04-02 14:33:05 +0530

    1

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px', fontFamily: 'Arial, sans-serif' }}>
+      <h1>Hello, I'm Saimanideep</h1>
+      <p>Contact me: Saimanideep765@gmail.com</p>
+      <p>Phone: 8125956877</p>
+    </div>
+  );
+}
+
+export default App;
